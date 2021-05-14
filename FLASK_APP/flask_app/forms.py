4262cb0e9aa04060a4096f9662d0e61f86f73290
subham103 <subhamsahoo4444@gmail.com>
2021-05-14 20:00:44 +0530

    add feature selection route

--- FLASK_APP/flask_app/forms.py
+++ FLASK_APP/flask_app/forms.py
@@ -6,6 +6,7 @@ from wtforms import (
     SelectField,
     StringField,
     SubmitField,
+    SelectMultipleField,
     TextAreaField,
 )
 from wtforms.validators import URL, DataRequired, Email, EqualTo, Length
@@ -17,6 +18,14 @@ class VisualizeForm(FlaskForm):
     address_A = StringField("address_A", [DataRequired()])
     address_B = StringField("address_B", [DataRequired()])
     sampling_rate = StringField("sampling_rate", [DataRequired()])
+    apply_filter = SelectField(
+        "apply_filter",
+        [DataRequired()],
+        choices=[
+            ("yes", "yes"),
+            ("no", "no"),
+        ],
+    )
     low_frequency = SelectField(
         "low_frequency",
         [DataRequired()],
@@ -50,5 +59,77 @@ class VisualizeForm(FlaskForm):
             ("blackman", "blackman"),
         ],
     )
-    # submit = SubmitField("Next", render_kw={"onclick": ""})
+    delta_frequency_initial = StringField("delta_frequency_initial", [DataRequired()])
+    delta_frequency_final = StringField("delta_frequency_final", [DataRequired()])
+    theta_frequency_initial = StringField("theta_frequency_initial", [DataRequired()])
+    theta_frequency_final = StringField("theta_frequency_final", [DataRequired()])
+    alpha_frequency_initial = StringField("alpha_frequency_initial", [DataRequired()])
+    alpha_frequency_final = StringField("alpha_frequency_final", [DataRequired()])
+    beta_frequency_initial = StringField("beta_frequency_initial", [DataRequired()])
+    beta_frequency_final = StringField("beta_frequency_final", [DataRequired()])
+    gamma_frequency_initial = StringField("gamma_frequency_initial", [DataRequired()])
+    gamma_frequency_final = StringField("gamma_frequency_final", [DataRequired()])
+    initial_datapoint = StringField("initial_datapoint", [DataRequired()])
+    final_datapoint = StringField("final_datapoint", [DataRequired()])
+
     submit = SubmitField("Next")
+
+
+class FeatureForm(FlaskForm):
+    """Feature Form"""
+    window_size = StringField("window_size", [DataRequired()])
+    feat_input = SelectMultipleField(
+        "feat_input",
+        choices=[
+            ("median_absolute_deviation", "median_absolute_deviation"),
+            ("kurtosis", "kurtosis"),
+            ("standard_dev", "standard_dev"),
+            ("integration", "integration"),
+            ("variance", "variance"),
+            ("mean", "mean"),
+            # ("app_entropy", "app_entropy"),
+            ("hurst_expo", "hurst_expo"),
+            ("detrended_fluctuation", "detrended_fluctuation"),
+            ("sample_entropy", "sample_entropy"),
+            ("correlation_dim", "correlation_dim"),
+            ("lyapunov_expo", "lyapunov_expo"),
+        ],
+    )
+    embedded_dim_sample_entropy = StringField("embedded_dim_sample_entropy")
+    embedded_dim_correlation_dim = StringField("embedded_dim_correlation_dim")
+    embedded_dim_lyapunov_expo = StringField("embedded_dim_lyapunov_expo")
+    feat_selection_method = SelectField(
+        "feat_selection_method",
+        [DataRequired()],
+        choices=[
+            ("SelectPercentile", "SelectPercentile"),
+            ("SelectKBest", "SelectKBest"),
+            ("none", "none"),
+        ],
+    )
+    percentage_or_value = StringField("percentage_or_value", [DataRequired()])
+    cv_fold = StringField("cv_fold", [DataRequired()])
+    k_fold_index = SelectField(
+        "k_fold_index",
+        [DataRequired()],
+        choices=[
+            ("0", "KFold"),
+            ("1", "StratifiedKFold"),
+        ],
+    )
+    classifier_index = SelectField(
+        "classifier_index",
+        [DataRequired()],
+        choices=[
+            ("0", "SVC"),
+            ("1", "SGDClassifier"),
+            ("2", "KNeighborsClassifier"),
+            ("3", "GaussianProcessClassifier"),
+            ("4", "GaussianNB"),
+            ("5", "DecisionTreeClassifier"),
+            ("6", "AdaBoostClassifier"),
+            ("7", "GradientBoostingClassifier"),
+        ],
+    )
+
+    run = SubmitField("Run")
